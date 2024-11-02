dc525bf6d41b4819ac6217f315be57b6c7efeb1d
ZQW <phyer219@gmail.com>
2024-11-02 16:55:17 +0800

    add ipynb support

--- plugins/org_md_zqw_render/org_md_zqw_render.py
+++ plugins/org_md_zqw_render/org_md_zqw_render.py
@@ -5,6 +5,8 @@ from .org_render import convert_to_html
 from markdown import Markdown
 from pelican.readers import MarkdownReader
 from jinja2 import ChoiceLoader, DictLoader, PrefixLoader
+from nbconvert.exporters import HTMLExporter
+import nbformat
 
 
 class OrgReader(BaseReader):
@@ -22,6 +24,33 @@ class OrgReader(BaseReader):
     def read(self, source_path):
         content,  original_meta = convert_to_html(source_path)
         metadata = self._parse_metadata(original_meta)
+        return content, metadata
+
+
+class IpynbReader(BaseReader):
+    """Reader for Org files"""
+    enabled = True
+    file_extensions = ['ipynb']
+
+    def _parse_metadata(self, meta):
+        if 'categories' in meta:
+            meta['category'] = meta['categories']
+        output = {key: self.process_metadata(key, value)
+                  for key, value in meta.items()}
+        return output
+
+    def read(self, source_path):
+        default_template_name = 'plugins/org_md_zqw_render/my_jupyter_template'
+        template_name = default_template_name
+
+        with open(source_path) as f:
+            nb = nbformat.read(f, as_version=4)
+        original_meta = nb.cells[0].metadata
+        metadata = self._parse_metadata(original_meta)
+        print(original_meta)
+        exporter = HTMLExporter(theme='dark',
+                                template_name=template_name)
+        content, _ = exporter.from_notebook_node(nb)
         return content, metadata
 
 
@@ -56,6 +85,7 @@ def add_reader(readers):
     for ext in OrgReader.file_extensions:
         readers.reader_classes[ext] = OrgReader
     readers.reader_classes['md'] = MyMDReader
+    readers.reader_classes['ipynb'] = IpynbReader
 
 
 def modify_template(generator):
