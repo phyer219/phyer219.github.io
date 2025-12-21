b1f7e536d3f0081db94b880e0851e83afd37c046
Zeqing Wang <phyer219@gmail.com>
2025-12-21 15:40:06 +0900

    test rss

--- publishconf.py
+++ publishconf.py
@@ -9,10 +9,10 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://zqw.ink"
-# RELATIVE_URLS = False
+RELATIVE_URLS = False
 
-# FEED_ALL_ATOM = "feeds/all.atom.xml"
-# CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 # DELETE_OUTPUT_DIRECTORY = True
 
